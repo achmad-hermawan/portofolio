37158cb6db24f2adb7b4962264039d198693932d
achmad-hermawan <achmad.hermawan@utdi.ac.id>
2025-11-12 11:30:05 +0700

    Feat : build to PWA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 // =============================
+// DAFTARKAN SERVICE WORKER
+// =============================
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker berhasil didaftarkan:', registration);
+      })
+      .catch((error) => {
+        console.log('Pendaftaran Service Worker gagal:', error);
+      });
+  });
+}
+
+// =============================
 // NAVBAR ACTIVE SCROLL
 // =============================
 const sections = document.querySelectorAll("section");
---
+++ images/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/script.js',
+  '/images/profil.jpg',
+  '/images/icons/profil.jpg',
+  // Tambahkan file-file penting lainnya (misalnya, gambar proyek) jika perlu
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache dibuka');
+        return cache.addAll(urlsToCache); 
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request); 
+      })
+  );
+});
